421cb381be9e19f0f5b73588eff725510c7d2657
cosmodro <alexangulo9@gmail.com>
2025-09-16 10:46:09 -0600

    updates

--- scripts/sort_gallery.py
+++
@@ -1,6 +0,0 @@
-"""Removed: static sorter was removed per user request.
-
-This file previously contained a script to statically sort gallery cards. The
-script was removed on {date}. If you need a sorter again, re-create the
-script or ask to restore it from the repository history.
-""".format(date='2025-09-16')
